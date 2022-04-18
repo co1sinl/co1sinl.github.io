b77eb6a2cc45ec565f467c96ab0eb0c266b3a1d8
co1sinl <co1sinl@163.com>
2022-04-18 23:22:57 +0800

    Site updated: 2022-04-18 23:22:55

---
+++ js/baidu.tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d91421cf90688b8ec1aaf70076582b32";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
